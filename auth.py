a8cbee33b1350fff1e0896b8e9e44a779d644bc0
vinhvinhcuteee <vinhvinhcuteee@gmail.com>
2026-04-23 13:30:14 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,7 +1,6 @@
 from functools import wraps
-from flask import session, redirect, url_for, request
+from flask import session, redirect, url_for
 
-# Thông tin đăng nhập
 ADMIN_USERNAME = "hoangvinh"
 ADMIN_PASSWORD = "Vohoangvinh0807"
 
